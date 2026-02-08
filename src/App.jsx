4a11483b024f0fd5008632be9817d74bb8343c18
arielmarno <arielmarno@outlook.com>
2026-02-08 20:46:03 -0300

    Elimina componentes no utilizados y mejora la estructura del componente Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import ScrollToHash from './components/ScrollToHash';
-import Whatsapp from './components/Whatsapp';
-import Footer from './components/Footer';
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import TermsAndConditions from './pages/TermsAndConditions';
@@ -13,14 +11,12 @@ function App() {
   return (
     <div>
       <ScrollToHash />
-      <Whatsapp />
       <Routes >
         <Route path="/" element={<Home/>}/>
         <Route path="/contacto" element={<Contact/>}/>
         <Route path="/terminosycondiciones" element={<TermsAndConditions/>}/>
         <Route path="/politicadeprivacidad" element={<Policy/>}/>
       </Routes>
-      <Footer />
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
+import React from 'react';
 import '../styles/pages/home.css';
 import arrow from '../assets/arrow-white.webp';
 import Liquid from '../components/Liquid';
 import Platforms from '../components/Platforms';
 import Navbar from '../components/Navbar';
+import Whatsapp from '../components/Whatsapp';
 
-//iconos plataformas
+// Iconos plataformas
 import google from '../assets/platforms/google-black.webp';
 import meta from '../assets/platforms/meta-black.webp';
 import nube from '../assets/platforms/nube-black.webp';
 import ad from '../assets/platforms/ad-black.webp';
 
-//componentes secciones
+// Otros componentes
 import AboutUs from '../components/AboutUs';
 import HomePortfolio from '../components/HomePortfolio';
 import DesignDev from '../components/services/DesignDev';
@@ -19,58 +21,57 @@ import AdsContent from '../components/services/AdsContent';
 import Ecommerce from '../components/services/Ecommerce';
 import Clients from '../components/Clients';
 import HowWeWork from '../components/HowWeWork';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <>
+    <div className='home-component'>
       <Navbar />
-      <div className='home' id='inicio'>
-        <div className='liquid'>
+      <Whatsapp />
+      
+      <section className='hero-section' id='inicio'>
+        <div className='liquid-bg'>
           <Liquid
             mouseForce={20}
             cursorSize={50}
             isViscous={false}
-            viscous={5}
             colors={["#2C4499", "#FF9FFC", "#B19EEF"]}
             autoDemo={true}
             autoSpeed={0.1}
             autoIntensity={0.1}
-            isBounce={false}
             resolution={0.20}
-            takeoverDuration={0.10}
-            autoResumeDelay={1000}
-            autoRampDuration={0.3}
           />
         </div>
-        <main>
+
+        <div className='hero-content'>
           <article className='title'>
             <h1>Visibilidad real para tu negocio</h1>
-            <h4>Generamos <span>interés y confianza</span> en tu marca mostrándola <span>de la forma correcta</span>. <br />Nos ocupamos de acercarte a tu público para que te encargues de lo más importante.</h4>
-            <a href="/#portafolio" className='cta-btn_white'>Nuestros trabajos <img src={arrow} alt="flecha"/></a>
+            <h4>
+              Generamos <span>interés y confianza</span> en tu marca mostrándola <span>de la forma correcta</span>. 
+              <br />Nos ocupamos de acercarte a tu público para que te encargues de lo más importante.
+            </h4>
+            <a href="/#portafolio" className='cta-btn_white'>
+              Nuestros trabajos <img src={arrow} alt="flecha"/>
+            </a>
           </article>
-          <section>
-            <div className='box'>
-              <div className='item'>
-                <img className='google' src={google} alt="google negocios" />
-              </div>
-              <div className='item'>
-                <img className='meta' src={meta} alt="meta" />
-              </div>
+
+          {/* Logos estáticos - Solo visibles en Desktop */}
+          <div className='desktop-platforms'>
+            <div className='logo-row'>
+                <img className='img-google' src={google} alt="google" />
+                <img className='img-meta' src={meta} alt="meta" />
+                <img className='img-nube' src={nube} alt="tienda nube" />
+                <img className='img-ad' src={ad} alt="google ads" />
             </div>
-            <div className='box'>
-              <div className='item'>
-                <img className='nube' src={nube} alt="tienda nube" />
-              </div>
-              <div className='item'>
-                <img className='ad' src={ad} alt="publicidad de google" />
-              </div>
-            </div>
-          </section>
-        </main>
-        <div className='platforms-comp'>
-            <Platforms />
+          </div>
+
+          {/* Banda transcurriendo - Solo visible en Mobile */}
+          <div className='mobile-platforms-band'>
+              <Platforms />
+          </div>
         </div>
-      </div>
+      </section>
+
       <AboutUs />
       <HomePortfolio />
       <DesignDev />
@@ -79,8 +80,9 @@ const Home = () => {
       <Ecommerce />
       <Clients />
       <HowWeWork />
-    </>
-  )
+      <Footer />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
